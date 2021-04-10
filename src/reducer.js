6db719b4be518dd7e8bc060011dcff1247bb1f60
Heltzel <heltzel.service@hotmail.com>
2021-04-10 16:21:15 +0200

    Simple dispatch

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,26 @@
-import { INCREASE, DECREASE } from './actions'
+import { INCREASE, DECREASE, CLEAR_CART, REMOVE } from './actions'
 
 function reducer(state, action) {
-  return state
+  switch (action.type) {
+    case CLEAR_CART:
+      return { ...state, cart: [] }
+    case DECREASE:
+      console.log('You decreased the amount')
+      break
+    case INCREASE:
+      console.log('You increased the amount')
+      break
+    case REMOVE:
+      console.log('You removed the item')
+      break
+    default:
+      return state
+  }
+
+  // if (action.type === CLEAR_CART) {
+  //   return { ...state, cart: [] }
+  // }
+  // return state
 }
 
 export default reducer
