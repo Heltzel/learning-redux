b1f1b5e2fac159990321edddf8413be57a965405
Heltzel <heltzel.service@hotmail.com>
2021-04-10 22:20:30 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import React from 'react'
 // components
 import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
-// items
-import cartItems from './cart-items'
 
 import { createStore } from 'redux'
 import reducer from './reducer'
 // react-redux - Provider wraps app, connect - used in components
 import { Provider } from 'react-redux'
 
-//initial store
-const intialStore = {
-  cart: cartItems,
-  total: 99.99,
-  amount: 5,
-}
-
 // store.getState() -
 // reducer here is imported at the top
-const store = createStore(reducer, intialStore)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
 
 function App() {
   return (
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,22 @@
-import { INCREASE, DECREASE, CLEAR_CART, REMOVE } from './actions'
+// items
+import cartItems from './cart-items'
+import {
+  INCREASE,
+  DECREASE,
+  CLEAR_CART,
+  REMOVE,
+  GET_TOTALS,
+  TOGGLE_AMOUNT,
+} from './actions'
 
-function reducer(state, action) {
+//initial store
+const intialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+}
+
+function reducer(state = intialStore, action) {
   const { payload } = action
   let tempCart = []
 
@@ -8,17 +24,13 @@ function reducer(state, action) {
     case CLEAR_CART:
       return { ...state, cart: [] }
     case DECREASE:
-      tempCart = []
-      if (payload.amount === 1) {
-        tempCart = state.cart.filter((item) => item.id !== payload.id)
-      } else {
-        tempCart = state.cart.map((item) => {
-          if (item.id === payload.id) {
-            item = { ...item, amount: item.amount - 1 }
-          }
-          return item
-        })
-      }
+      tempCart = state.cart.map((item) => {
+        if (item.id === payload.id) {
+          item = { ...item, amount: item.amount - 1 }
+        }
+        return item
+      })
+
       return { ...state, cart: tempCart }
     case INCREASE:
       tempCart = state.cart.map((item) => {
@@ -32,6 +44,37 @@ function reducer(state, action) {
       return {
         ...state,
         cart: state.cart.filter((item) => item.id !== payload.id),
+      }
+    case GET_TOTALS:
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem
+          const itemTotal = price * amount
+          cartTotal.total += itemTotal
+          cartTotal.amount += amount
+          return cartTotal
+        },
+        {
+          total: 0,
+          amount: 0,
+        },
+      )
+      total = parseFloat(total.toFixed(2))
+      return { ...state, total, amount }
+    case TOGGLE_AMOUNT:
+      return {
+        ...state,
+        cart: state.cart.map((cartItem) => {
+          if (cartItem.id === payload.id) {
+            if (payload.toggle === 'inc') {
+              return (cartItem = { ...cartItem, amount: cartItem.amount + 1 })
+            }
+            if (payload.toggle === 'dec') {
+              return (cartItem = { ...cartItem, amount: cartItem.amount - 1 })
+            }
+          }
+          return cartItem
+        }),
       }
     default:
       return state
