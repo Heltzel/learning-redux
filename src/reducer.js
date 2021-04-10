0af61988679bd09d0ab6aaa4398d5b73b33c6b50
Heltzel <heltzel.service@hotmail.com>
2021-04-10 17:41:02 +0200

    Increase and decrease amount of items

--- src/reducer.js
+++ src/reducer.js
@@ -2,17 +2,33 @@ import { INCREASE, DECREASE, CLEAR_CART, REMOVE } from './actions'
 
 function reducer(state, action) {
   const { payload } = action
+  let tempCart = []
+
   switch (action.type) {
     case CLEAR_CART:
       return { ...state, cart: [] }
     case DECREASE:
-      console.log('You decreased the amount')
-      return state
+      tempCart = []
+      if (payload.amount === 1) {
+        tempCart = state.cart.filter((item) => item.id !== payload.id)
+      } else {
+        tempCart = state.cart.map((item) => {
+          if (item.id === payload.id) {
+            item = { ...item, amount: item.amount - 1 }
+          }
+          return item
+        })
+      }
+      return { ...state, cart: tempCart }
     case INCREASE:
-      console.log('You increased the amount')
-      return state
+      tempCart = state.cart.map((item) => {
+        if (item.id === payload.id) {
+          item = { ...item, amount: item.amount + 1 }
+        }
+        return item
+      })
+      return { ...state, cart: tempCart }
     case REMOVE:
-      console.log(payload.id)
       return {
         ...state,
         cart: state.cart.filter((item) => item.id !== payload.id),
